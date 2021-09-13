b7f09654812aa93a16dd9f7b8c34670e6160fe06
iagokrt <iago_floripa@hotmail.com>
2021-09-13 02:39:29 -0300

    firsting passing test

---
+++ ChCallback.js
@@ -0,0 +1,3 @@
+let nums = [1, 2, 3, 5, 7, 9];
+
+export default nums
--- test/callback.test.js
+++ test/callback.test.js
@@ -1,5 +1,7 @@
 import { expect } from 'chai'
 
+import nums from '../ChCallback'
+
 describe('ch_callback', () => {
     it('should return an array of numbers', () => {
         expect(nums).to.be.an('array')
