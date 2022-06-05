71675248beb3d5023471d0f807a9eb9fcbddf059
iagokrt <iago_floripa@hotmail.com>
2022-06-05 01:50:19 -0300

    new bees

--- dev/script.js
+++ dev/script.js
@@ -1,12 +1,10 @@
-var input = require('fs').readFileSync('./dev/stdin', 'utf8')
+var input=require('fs').readFileSync('stdin','utf8')
+var [t,p]=input.split('\n')
+if(!p)return console.log(0)
+var tea=parseInt(t)
+var total=0
+p.split(' ').map((n)=>{
+    if(parseInt(n) === tea)total++
+})
+console.log(total)
 
-var [a, b] = input.split(' ').map((n)=> parseInt(n));
-var response = '';
-
-if (a % b == 0) {
-    response = 'Sao Multiplos'
-}  else {
-    response = 'Nao sao Multiplos'
-}
-
-export default response
