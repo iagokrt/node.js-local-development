0fe43006df668348f40f0317152a8e1cccda75c9
iagokrt <iago_floripa@hotmail.com>
2022-06-05 01:50:30 -0300

    added new bees

--- dev/script.js
+++ dev/script.js
@@ -1,10 +1,1 @@
 var input=require('fs').readFileSync('stdin','utf8')
-var [t,p]=input.split('\n')
-if(!p)return console.log(0)
-var tea=parseInt(t)
-var total=0
-p.split(' ').map((n)=>{
-    if(parseInt(n) === tea)total++
-})
-console.log(total)
-
