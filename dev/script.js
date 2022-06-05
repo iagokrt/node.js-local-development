c5886711feb9cc0f0feb52424ff8f5bb1c05b28f
iagokrt <iago_floripa@hotmail.com>
2022-06-05 17:28:21 -0300

    organize bees. moving files bbzbzzzz

--- dev/script.js
+++ dev/script.js
@@ -1,10 +1,4 @@
 var input=require('fs').readFileSync('stdin','utf8')
 
-var [n,m] = input.split('\n').map((n) => parseInt(n));
-console.log(n-m);
-// Escreva um programa que, 
-// dada a pressão desejada digitada
-// pelo motorista 
-// e a pressão do pneu lida pela bomba, 
-
-// indica a diferença entre a pressão desejada e a pressão lida.
+// var [n,m] = input.split('\n').map((n) => parseInt(n));
+console.log(input);
