69a1ffdfdfd609aec8bfbaeb9e6fd182ffcbea3a
Iago Barreto <iago_floripa@hotmail.com>
2023-06-18 03:18:20 -0300

    apply chatgpt possible solution for the tests written by chatgpt's tdd

--- dev/leet_2357_make_array_zero.js
+++ dev/leet_2357_make_array_zero.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
-    return nums
-};
+    if (nums.length === 0) {
+      return 0;
+    }
+  
+    let min = Infinity;
+    for (const num of nums) {
+      if (num > 0 && num < min) {
+        min = num;
+      }
+    }
+  
+    let operations = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > 0) {
+        const subtract = Math.floor(nums[i] / min);
+        operations += subtract;
+        nums[i] -= subtract * min;
+      }
+    }
+  
+    return operations;
+  }
 
 export default minimumOperations;
