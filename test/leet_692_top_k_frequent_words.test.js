2fd63dbd6dc3307ef6ae52346b83ad53c7fbe219
Iago Barreto <iago_floripa@hotmail.com>
2023-06-19 00:53:45 -0300

    testing archiving.

--- test/leet_692_top_k_frequent_words.test.js
+++ test/leet_692_top_k_frequent_words.test.js
@@ -11,24 +11,24 @@ describe('692 - Top K Frequent Words ', (params) => {
     
     // problem setup test
     describe('Test the input arguments', () => {
-        it('Should be a function', () => {
-            expect(topKFrequent).to.be.a('function');
-        });
+        // it('Should be a function', () => {
+        //     expect(topKFrequent).to.be.a('function');
+        // });
     });
 
     // program test
     describe('Run tests on main program', () => {
-        it('Should return the k most frequent words', () => {
-            const words1 = ["i","love","leetcode","i","love","coding"];
-            const k1 = 2;
-            expect(topKFrequent(words1, k1)).to.deep.equal(["i", "love"]);
+        // it('Should return the k most frequent words', () => {
+        //     const words1 = ["i","love","leetcode","i","love","coding"];
+        //     const k1 = 2;
+        //     expect(topKFrequent(words1, k1)).to.deep.equal(["i", "love"]);
         
-            const words2 = ["the","day","is","sunny","the","the","the","sunny","is","is"];
-            const k2 = 4;
-            expect(topKFrequent(words2, k2)).to.deep.equal(["the", "is", "sunny", "day"]);
+        //     const words2 = ["the","day","is","sunny","the","the","the","sunny","is","is"];
+        //     const k2 = 4;
+        //     expect(topKFrequent(words2, k2)).to.deep.equal(["the", "is", "sunny", "day"]);
         
-            // Add more test cases as needed
-        });
+        //     // Add more test cases as needed
+        // });
 
     });
 
