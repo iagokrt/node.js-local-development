77f1348ae27684063c6520d4ef04c6490d7504f0
Iago Barreto <iago_floripa@hotmail.com>
2023-06-18 21:26:14 -0300

    added leet frequent words frequency map hashing

---
+++ dev/leet_692_top_k_frequent_words.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    // most frequent elements
+        
+    // 1 Create a frequency map
+        
+    // 2 Convert frequency map to array of [word, frequency] pairs
+    // 3 Sort the array based on frequency and lexicographical order
+
+    // 4 Extract the first k most frequent words
+
+    return result;
+};
+
+export default topKFrequent;
+
---
+++ test/leet_692_top_k_frequent_words.test.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+
+import topKFrequent from '../dev/leet_692_top_k_frequent_words'
+
+describe('692 - Top K Frequent Words ', (params) => {
+    /* params is generic based on the program definition 
+    / if is chosing to use a function approach to the solution, use this options */
+
+    // Expected output(s)/return:
+    // 
+    
+    // problem setup test
+    describe('Test the input arguments', () => {
+        it('Should be a function', () => {
+            expect(topKFrequent).to.be.a('function');
+        });
+    });
+
+    // program test
+    describe('Run tests on main program', () => {
+        it('Should return the k most frequent words', () => {
+            const words1 = ["i","love","leetcode","i","love","coding"];
+            const k1 = 2;
+            expect(topKFrequent(words1, k1)).to.deep.equal(["i", "love"]);
+        
+            const words2 = ["the","day","is","sunny","the","the","the","sunny","is","is"];
+            const k2 = 4;
+            expect(topKFrequent(words2, k2)).to.deep.equal(["the", "is", "sunny", "day"]);
+        
+            // Add more test cases as needed
+        });
+
+    });
+
+   
+})
