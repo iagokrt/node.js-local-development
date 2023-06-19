f44d6db281ebc44bad830136c5783c0f14bbb265
Iago Barreto <iago_floripa@hotmail.com>
2023-06-18 21:29:13 -0300

    added solution for frequent words

--- dev/leet_692_top_k_frequent_words.js
+++ dev/leet_692_top_k_frequent_words.js
@@ -5,15 +5,32 @@
  */
 var topKFrequent = function(words, k) {
     // most frequent elements
-        
+    
     // 1 Create a frequency map
-        
+    const frequencyMap = new Map();
+    for (const word of words) {
+        frequencyMap.set(word, (frequencyMap.get(word) || 0) + 1);
+    }
+
     // 2 Convert frequency map to array of [word, frequency] pairs
+    const frequencyArray = Array.from(frequencyMap);
+    //   console.log(frequencyArray);
+
     // 3 Sort the array based on frequency and lexicographical order
+    frequencyArray.sort((a, b) => {
+        if (a[1] !== b[1]) {
+            return b[1] - a[1]; // Sort by frequency if different
+        } else {
+            return a[0].localeCompare(b[0]); // Sort lexicographically if frequency is equal
+        }
+    });
+    // console.log(`new: ${typeof frequencyArray}`, frequencyArray);
+
 
     // 4 Extract the first k most frequent words
+    const result = frequencyArray.slice(0, k).map(pair => pair[0]);
 
-    return result;
+  return result;
 };
 
 export default topKFrequent;
